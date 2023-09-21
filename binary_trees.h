0f64de19be22920352c69603d2b5e40746f0dce4
KhairiTab <taboubikhairi68@gmail.com>
2023-09-21 19:41:02 +0100

    test

--- binary_trees.h
+++ binary_trees.h
@@ -1,5 +1,5 @@
-#ifndef BINARY_TREES_H
-#define BINARY_TREES_H
+#ifndef __BINARY_TREES_H__
+#define __BINARY_TREES_H__
 
 #include <stdio.h>
 #include <stddef.h>
@@ -19,10 +19,5 @@ typedef struct binary_tree_s binary_tree_t;
 void binary_tree_print(const binary_tree_t *);
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
-binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
-void binary_tree_delete(binary_tree_t *tree);
-int binary_tree_is_leaf(const binary_tree_t *node);
-int binary_tree_is_root(const binary_tree_t *node);
-void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+
 #endif
